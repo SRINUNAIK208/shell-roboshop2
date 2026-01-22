2b585454d419dba426f92c142468f6e937ad8dd9
SRINUNAIK208 <srinaik208@gmail>
2026-01-22 22:45:23 +0530

    shellroboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ Domain_Name="srinunayak.online"
 
 for instance in "$@"
 do
- Instance_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-00028162e61c10305 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+ Instance_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0c816d02911d3893c --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
       IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
